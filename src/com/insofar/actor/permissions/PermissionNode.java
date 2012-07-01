3667721b60c68bc52f3379a2531405da651647ee
joshw <joshw@insofar.com>
2012-06-30 20:50:15 -0600

    added troupe permission

--- src/com/insofar/actor/permissions/PermissionNode.java
+++ src/com/insofar/actor/permissions/PermissionNode.java
@@ -2,12 +2,19 @@ package com.insofar.actor.permissions;
 
 public enum PermissionNode
 {
-	COMMAND_RECORD(".command.record"), COMMAND_CUT(".command.cut"),
-		COMMAND_HIRE(".command.hire"), COMMAND_DUB(".command.dub"),
-		COMMAND_FIRE(".command.fire"), COMMAND_ACTION(".command.action"),
-		COMMAND_ACTIONREC(".command.actionrec"), COMMAND_LOOP(".command.loop"),
-		COMMAND_RESET(".command.reset"), COMMAND_SAVE_ACTOR(".command.saveactor"),
-		COMMAND_SAVE_SCENE(".command.savescene"), COMMAND_LOAD_ACTOR(".command.loadactor"),
+	COMMAND_RECORD(".command.record"),
+		COMMAND_CUT(".command.cut"),
+		COMMAND_HIRE(".command.hire"),
+		COMMAND_DUB(".command.dub"),
+		COMMAND_FIRE(".command.fire"),
+		COMMAND_ACTION(".command.action"),
+		COMMAND_ACTIONREC(".command.actionrec"),
+		COMMAND_LOOP(".command.loop"),
+		COMMAND_RESET(".command.reset"),
+		COMMAND_TROUPE(".command.troupe"),
+		COMMAND_SAVE_ACTOR(".command.saveactor"),
+		COMMAND_SAVE_SCENE(".command.savescene"),
+		COMMAND_LOAD_ACTOR(".command.loadactor"),
 		COMMAND_LOAD_SCENE(".command.loadscene");
 
 	private static final String prefix = "Actor";
