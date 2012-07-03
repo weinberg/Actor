3203cecca9c10deca3ac6b997692eb8246bb790b
joshw <joshw@insofar.com>
2012-07-02 22:30:16 -0600

    troupe support

--- src/com/insofar/actor/commands/author/ActionRecord.java
+++ src/com/insofar/actor/commands/author/ActionRecord.java
@@ -1,7 +1,14 @@
 package com.insofar.actor.commands.author;
 
+import java.util.HashMap;
+
+import org.bukkit.entity.Player;
+
+import com.insofar.actor.ActorAPI;
 import com.insofar.actor.ActorPlugin;
+import com.insofar.actor.Author;
 import com.insofar.actor.EntityActor;
+import com.insofar.actor.Recording;
 import com.insofar.actor.permissions.PermissionHandler;
 import com.insofar.actor.permissions.PermissionNode;
 
@@ -36,6 +43,39 @@ public class ActionRecord extends AuthorBaseCommand {
 					+ PermissionNode.COMMAND_ACTIONREC.getNode());
 			return true;
 		}
+		
+		// Record part
+		
+		Author author = ActorAPI.getAuthor(player);
+		if (author.getTroupeMembers().size() < 1)
+		{
+			player.sendMessage("No troupe members to record.");
+			return true;
+		}
+		
+		author.setRecording(true);
+		
+		HashMap<String,Recording> recMap = author.getTroupRecMap();
+		for (Player member : author.getTroupeMembers())
+		{
+			// If no recording exists make one
+			Recording r = recMap.get(member.getName());
+			if (r == null)
+			{
+				r = new Recording();
+				recMap.put(member.getName(), r);
+			}
+
+			ActorAPI.record(member, author.getTroupRecMap().get(member.getName()));
+			
+			if (member != player)
+				member.sendMessage(ActorPlugin.TAG + "Recording started");
+		}
+		
+		player.sendMessage("Recording started.");
+		
+		// Action part
+		
 		String actorName = args.length > 1 ? args[1] : "";
 		
 		for (EntityActor actor : plugin.actors)
@@ -46,8 +86,6 @@ public class ActionRecord extends AuthorBaseCommand {
 				actor.setIsPlayback(true);
 			}
 		}
-		
-		ActorPlugin.getInstance().record(player);
 		
 		return true;
 	}
